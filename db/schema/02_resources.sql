a527355fe50a1a1b7d1972548e9b42ef77b64fc2
Jacob1225 <carlone.jacob@gmail.com>
2020-05-02 19:00:24 +0000

    Created the tables for the database and seeded the database

---
+++ db/schema/02_resources.sql
@@ -0,0 +1,13 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS resources CASCADE;
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  url TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  keyword TEXT,
+  category VARCHAR(255) NOT NULL
+);
---
+++ db/schema/03_resource_reviews.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS resource_reviews CASCADE;
+CREATE TABLE resource_reviews (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    like BOOLEAN DEFAULT FALSE,
+    resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+    comment TEXT,
+    rating SMALLINT,
+    pin BOOLEAN DEFAULT FALSE, 
+);
