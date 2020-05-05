657eb7c9559f0e4e15385fdf616a744c789f18cd
Judith Huynh <judithuynh@gmail.com>
2020-05-05 02:48:17 +0000

    working on homepage routes, got login to redirect, need to work on the search routes

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@
 
 const express = require('express');
 const router  = express.Router();
-const bodyParser = require("body-parser");
-const PORT = 8080; // default port 8080
+// const bodyParser = require("body-parser");
+// const PORT = 8080; // default port 8080
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
