03bf204da1537c4adfc2f6a9b720f709d76af135
Jacob1225 <carlone.jacob@gmail.com>
2020-05-06 19:00:49 +0000

    Fixed registration route and adds a user to the database when registering

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,65 @@
 const express = require('express');
 const router  = express.Router();
-// const { Pool } = require("pg");
-// const bcrypt = require('bcrypt');
-// const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
 
-// router.use(cookieSession({
-//   name: 'session',
-//   keys: ['12fasf5ywefgd']
-// }));
+router.use(cookieSession({
+name: 'session',  keys: ['12fasf5ywefgd']
+}));
+
 module.exports =(db) => {
 
+  const getUserwithEmail = function(email) {
+      const queryString = 'SELECT * FROM users WHERE email = $1';
+      return db
+      .query(queryString, [email])
+      // .then(res => (console.log('brooooo', res.rows[0])))
+    }
+    
+  const getUserWithUsername = function(username) {
+    const queryString = `SELECT * FROM users WHERE username = $1`;
+    return db
+    .query(queryString, [username])
+    // .then(res => (console.log('beep', res.rows[0])))
+  }
 
-router.post('/', (res, req) => {
-  // const {email, password} = req.body;
-  // console.log(email);
-  // console.log(`password is ${password}`);
-  const username = req.body;
-  console.log(req.query)
-  console.log('Im the user on register', username)
+  const addUser = function (username, email, password, name, avatar) {
+    const queryString = `
+      INSERT INTO users (username, email, password, full_name, avatar)
+      VALUES ($1, $2, $3, $4, $5) RETURNING *
+    `
+    return db
+      .query(queryString, [username, email, password, name, avatar])
+      .then(res => (res.rows[0]))
+}
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    const name = user.fullname;
+    const email = user.email;
+    const avatar = user.avatar;
+    const username = user.username;
+    const password = user.password;
+    const hashedPassword = bcrypt.hashSync(password, 12);
+
+    if (!email || !password ||!username) {
+      res.status(400).send("Fields cannot be blank!");
+    }
+       
+    Promise
+      .all([getUserwithEmail(email), getUserWithUsername(username)])   
+      .then(([resultByEmail, resultByUsername]) => {
+        if(resultByEmail.rowCount || resultByUsername.rowCount){
+          res.status(400).send("An account with this email or username already exists!");
+        } else {
+          addUser(username, email, hashedPassword, name, avatar).then(user => {
+            req.session.userId = user.id;
+            res.redirect(`/users/login/${username}`)
+          })
+        }
+      })
 })
 
-return router
+    return router
 };
+
