c00343e545c0c92d1756d291f8a3311aa8938306
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-05-06 00:07:53 +0000

    Added to database

---
+++ routes/single_resource.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+
+  const getSingleResource = function(resourceID) {
+    let queryString = `
+    SELECT resources.*, AVG(resource_reviews.rating) AS rating, COUNT(resource_reviews.liking) AS likes
+    FROM resources
+    JOIN resource_reviews ON resource_id = resources.id
+    WHERE resources.category = 'Food'
+    GROUP BY resources.id;
+    `
+    return db
+    .query(queryString, resourceID)
+    .then(res => console.log('this is res:', res.rows))
+    // .then(res => res.rows)
+    .catch((err) => console.error(err));
+  }
+
+
+  router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    getSingleResource(id)
+    .then (resources => {
+      console.log(resources);
+      res.render('resource', {resources})
+    })
+    .catch((err) => (res.status(500).send(err)));
+  })
+
+  return router;
+
+
+  const addLike = function(resourceID) {
+    let queryString =`
+    UPDATE resource_reviews
+    SET liking = TRUE
+    WHERE id = $1
+    `
+  }
+
+  const addComment = function(comment, resourceID) {
+    let queryString = `
+    UPDATE resource_reviews
+    SET comment = $1
+    WHERE id = $2
+    `
+  }
+
+  const addRating = function(rating, resourceID) {
+    let queryString = `
+    UPDATE resource_reviews
+    SET rating = $1
+    WHERE id = $2
+    `
+  }
+
+}
+
+//
